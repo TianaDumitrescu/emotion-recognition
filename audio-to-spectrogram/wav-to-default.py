678c9c62baa71e23fa4b43cb58723cee84e82d1e
Tiana Dumitrescu <tiana.dumitrescu@icloud.com>
2023-02-26 18:47:05 -0800

    Creates new folders with converted wav files

--- audio-to-spectrogram/wav-to-default.py
+++ audio-to-spectrogram/wav-to-default.py
@@ -1,15 +1,28 @@
 import glob
+import subprocess
+import os
 
-path = "data-sets/ravdess/"
-files=glob.glob(path+'**/*.wav')
+file_pattern = "data-sets/ravdess/**/*.wav"
+files=glob.glob(file_pattern)
 
-if len(files)==0:
+if len(files) == 0:
     print("No files to process.")
-    exit
+    exit()
+
+# Creates folders in converted-to-default-wav
+for file in files:
+
+    # Gets folder name
+    f = file.split("\\")
+    new_path = "data-sets/converted-to-default-wav/" + f[1]
+
+    # Creates folder if does not exist
+    if not os.path.isdir(new_path):
+        os.makedirs(new_path)
 
 for file in files:
-    mp=file
-    wa=file.replace('wav','default.wav')
-    print(wa)
-#subprocess.call(['ffmpeg', mp, '-e', 'mu-law','-r', '16k', wa, 'remix', '1,2'])
+    input_file = file
+    output_file = file.replace('wav','default.wav').replace('ravdess', 'converted-to-default-wav')
+    subprocess.Popen(['ffmpeg', '-i', input_file, output_file])
+
 print("Finished processing files.")
