3f6a77fe6d96776ebfd3d034df1fb29a2df15032
Tiana Dumitrescu <tiana.dumitrescu@icloud.com>
2023-02-27 17:54:44 -0800

    Creates emotion folders and sorts spectrograms by emotion into 'training' and 'testing'

--- audio-to-spectrogram/sort-spectrogram-by-emotion.py
+++ audio-to-spectrogram/sort-spectrogram-by-emotion.py
@@ -1,7 +1,10 @@
+from cgi import test
 from csv import field_size_limit
 import glob
 import os
+import random
 
+random.seed(10)
 file_pattern = "data-sets/converted-to-spectrogram/**/*.jpg"
 files=glob.glob(file_pattern)
 
@@ -37,11 +40,31 @@ for file in files:
         emotion = "surprised"
     
     input_file = input_file.replace("/", "\\");
-    output_file = file.replace(current_folder, emotion).replace('converted-to-spectrogram', 'final-data-set/training').replace("/", "\\");
-    copy_command = "copy /y """ + input_file + """ """ + output_file + "" ""
+    training_output_file = file.replace(current_folder, emotion).replace('converted-to-spectrogram', 'final-data-set/training').replace("/", "\\");
+    test_output_file = file.replace(current_folder, emotion).replace('converted-to-spectrogram', 'final-data-set/testing').replace("/", "\\");
+    
+    # Checks if folder for emotion exists, and creates if not (training data)
+    training_emotion_dir = "data-sets/final-data-set/training/" + emotion
+    if not os.path.isdir(training_emotion_dir):
+        os.makedirs(training_emotion_dir)
+
+
+    copy_command = "copy /y """ + input_file + """ """ + training_output_file + "" ""
     os.popen(copy_command)
     
+    if (random.random() < 0.5):
+        # Checks if folder for emotion exists, and creates if not (testing data)
+        testing_emotion_dir = "data-sets/final-data-set/testing/" + emotion
+        if not os.path.isdir(testing_emotion_dir):
+            os.makedirs(testing_emotion_dir)
+
+        copy_command = "copy /y """ + input_file + """ """ + test_output_file + "" ""
+        os.popen(copy_command)
+
+
+
     print("Original file: " + input_file)
-    print("New file:      " + output_file)
+    print("New file:      " + training_output_file)
     print(copy_command)
     print()
+
